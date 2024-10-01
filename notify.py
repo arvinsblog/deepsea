48250d77898512ab67f760a340ac1912a88f6a04
arvinsblog <62495855+arvinsblog@users.noreply.github.com>
2024-10-01 15:25:43 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -82,7 +82,7 @@ push_config = {
 
     'QYWX_KEY': '',                     # 企业微信机器人
 
-    'TG_BOT_TOKEN': '',                 # tg 机器人的 TG_BOT_TOKEN，例：1407203283:AAG9rt-6RDaaX0HBLZQq0laNOh898iFYaRQ
+    'TG_BOT_TOKEN': '',                 # tg 机器人的 TG_BOT_TOKEN，例：1407203283:xxxxxxxxxx
     'TG_USER_ID': '',                   # tg 机器人的 TG_USER_ID，例：1434078534
     'TG_API_HOST': '',                  # tg 代理 api
     'TG_PROXY_AUTH': '',                # tg 代理认证参数
